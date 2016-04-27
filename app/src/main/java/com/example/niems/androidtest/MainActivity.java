b09774917c8b6218e76debeffd5bbb7976cf7bd6
niems <czniemann1990@gmail.com>
2016-04-27 03:05:02 -0500

    colors :D

--- app/src/main/java/com/example/niems/androidtest/MainActivity.java
+++ app/src/main/java/com/example/niems/androidtest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitleColor(Color.BLUE);
     }
 
     public void sendMessage(View view){
